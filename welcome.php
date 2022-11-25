b66378168f4a0122e9e1604c599a56de4163720a
khudzu <64119084+khudzu@users.noreply.github.com>
2022-11-25 08:52:24 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -19,20 +19,40 @@ if ($conn->connect_error) {
 }
 
 // Create database
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE SCHEMA myDB";
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
   echo "Error creating database: " . $conn->error;
 }
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
+// sql to create table
+$sql = "CREATE TABLE myDB.MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+$sql = "INSERT INTO myDB.MyGuests (firstname, lastname, email)
 VALUES ('$firstname','$lastname', '$email');";
 
 if ($conn->query($sql) === TRUE) {
